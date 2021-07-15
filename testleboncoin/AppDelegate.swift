38d85f12d5495540e941ee7c1271725e21da8da9
ydeng <hotkakiphone@yahoo.fr>
2021-07-15 16:42:54 +0200

    Correction pour SwiftLint

--- testleboncoin/AppDelegate.swift
+++ testleboncoin/AppDelegate.swift
@@ -11,7 +11,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-
     func application(_: UIApplication,
                      didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
@@ -22,7 +21,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
-
-
 }
-
